cc6da4097216f28f6c6b8ec3c51311e3667eb875
Jun4928 <jun.choi.4928@gmail.com>
2024-08-22 11:41:50 +0200

    debug

--- scripts/branchCleanup.sh
+++ scripts/branchCleanup.sh
@@ -1,5 +1,13 @@
-git branch --list '*release*' --sort=-committerdate -av \
-  | tail +5 \
-  | awk '{print $1}' \
-  | sed 's/remotes\/origin\///' \
-  | xargs -L1 -I {} git push origin --delete {} --no-verify
+#!/bin/bash
+
+set -e
+set -x
+
+git branch --list '*release*' --sort=-committerdate -av | tee step1.log
+tail +5 step1.log | tee step2.log
+awk '{print $1}' step2.log | tee step3.log
+sed 's/remotes\/origin\///' step3.log | tee step4.log
+# xargs -L1 -I {} echo "git push origin --delete {} --no-verify" < step4.log | tee step5.log
+
+# Uncomment the following line to actually delete branches
+# xargs -L1 -I {} git push origin --delete {} --no-verify < step4.log
